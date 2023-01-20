ab148f02c49a2477ec1208a4cb3068637fb9d804
remon <blackshadowsoftwareltd@gmail.com>
2023-01-21 00:22:20 +0600

    chocolatey

--- color_converter/ui.dart
+++
@@ -1,8 +0,0 @@
-///? color to hex color
-
-const color = Colors.green;
-final hexColor = '#${color.value.toRadixString(16)}';
-
-///? hex to color
-
-Color color = hex.toColor();
--- image_height_width.dart
+++
@@ -1,4 +0,0 @@
-    final decodedImage =
-	    await decodeImageFromList(file.readAsBytesSync()); //? image from file
-	if (decodedImage.width != 100 ||
-	 decodedImage.height != 100) { }  
